436ce5bbb2e33a7208a89a2d5f6481d656c67519
Dimon <45654963+Khudienko@users.noreply.github.com>
2018-12-15 20:30:46 +0200

    Made few collections

---
+++ src/Skill.java
@@ -0,0 +1,13 @@
+public class Skill {
+    String skills;
+    public Skill(String str){
+        skills=str;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "skills='" + skills + '\'' +
+                '}';
+    }
+}
